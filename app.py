a8118ce8ae1911e59bb85fd56bab4a5cd441f69c
saehaana <61268356+saehaana@users.noreply.github.com>
2023-01-03 12:03:04 -0500

    created views.py to contain all routes

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, url_for
+from views import views
 
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('index.html')
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
     app.run(debug=True)
